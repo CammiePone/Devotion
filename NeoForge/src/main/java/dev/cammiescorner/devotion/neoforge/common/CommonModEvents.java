ca9abb11f1e2ae0a9d5e41993860db8bca3b33c3
CammiePone <inazuma261@gmail.com>
2025-02-22 23:50:22 -0800

    jsut moved textures to item/staff_parts

---
+++ NeoForge/src/main/java/dev/cammiescorner/devotion/neoforge/common/CommonModEvents.java
@@ -0,0 +1,9 @@
+package dev.cammiescorner.devotion.neoforge.common;
+
+import dev.cammiescorner.devotion.Devotion;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = Devotion.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+public class CommonModEvents {
+
+}
