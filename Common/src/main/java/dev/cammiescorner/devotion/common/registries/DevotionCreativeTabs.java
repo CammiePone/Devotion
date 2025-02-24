d865ae616e0e25c5572f1b9ae8beec62bfd61763
CammiePone <inazuma261@gmail.com>
2025-02-23 23:54:16 -0800

    change main tab to base tab

--- Common/src/main/java/dev/cammiescorner/devotion/common/registries/DevotionCreativeTabs.java
+++ Common/src/main/java/dev/cammiescorner/devotion/common/registries/DevotionCreativeTabs.java
@@ -15,7 +15,7 @@ import net.minecraft.world.item.ItemStack;
 public class DevotionCreativeTabs {
 	public static final RegistryHandler<CreativeModeTab> CREATIVE_TABS = RegistryHandler.create(Registries.CREATIVE_MODE_TAB, Devotion.MOD_ID);
 
-	public static final RegistrySupplier<CreativeModeTab> MAIN_TAB = CREATIVE_TABS.register("main_tab", () -> CreativeTabHelper.newBuilder(Devotion.id("main_tab")).icon(() -> new ItemStack(DevotionItems.BASIC_MAGE_HOOD.get())).displayItems((parameters, output) -> CreativeTabHelper.addRegistryEntries(parameters, output, DevotionItems.ITEMS)).build());
+	public static final RegistrySupplier<CreativeModeTab> BASE_TAB = CREATIVE_TABS.register("base_tab", () -> CreativeTabHelper.newBuilder(Devotion.id("base_tab")).icon(() -> new ItemStack(DevotionItems.BASIC_MAGE_HOOD.get())).displayItems((parameters, output) -> CreativeTabHelper.addRegistryEntries(parameters, output, DevotionItems.ITEMS)).build());
 	public static final RegistrySupplier<CreativeModeTab> STAFF_TAB = CREATIVE_TABS.register("staff_tab", () -> CreativeTabHelper.newBuilder(Devotion.id("staff_tab")).icon(() -> new ItemStack(DevotionItems.STAFF.get())).displayItems((parameters, output) -> {
 		for(Holder.Reference<StaffCore> core : DevotionStaffCores.REGISTRY.holders().toList()) {
 			ItemStack stack = new ItemStack(DevotionItems.STAFF_CORE.get());
